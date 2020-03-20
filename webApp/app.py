28dddd3bc1e5ebcd5e277da474cc7fccef27e1ac
Hamzah <hamzah18051@iiitd.ac.in>
2020-03-21 01:51:18 +0530

    js-python integration

---
+++ webApp/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, redirect, url_for,request
+from flask import make_response
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "hi"
+# @app.route("/index")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+   message = None
+   if request.method == 'POST':
+        datafromjs = request.form['mydata']
+        result = "Hello From Python"
+        resp = make_response('{"response": '+result+'}')
+        resp.headers['Content-Type'] = "application/json"
+        return resp
+        return render_template('loging.html', message='')
+
+@app.after_request
+def add_headers(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    return response
+
+if __name__ == "__main__":
+    # app.run(host="127.0.0.1", port="5000", debug = True)
+    # app.run(debug = True)
+    app.run(host='0.0.0.0', port="5000", debug=True)
